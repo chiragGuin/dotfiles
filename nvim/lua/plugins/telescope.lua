4b073d26a5efa3a4df425bb2653f1f234270d004
chiragGuin <chiragguin2008@gmail.com>
2025-11-04 22:39:06 +0530

    keymapping

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+  },
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
