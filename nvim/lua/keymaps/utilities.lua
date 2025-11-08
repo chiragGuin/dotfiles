781f23deae39a33e728ba40d0523ef7ee64e2098
chiragGuin <chiragguin2008@gmail.com>
2025-11-08 20:25:07 +0530

    snacks

--- nvim/lua/keymaps/utilities.lua
+++ nvim/lua/keymaps/utilities.lua
@@ -43,17 +43,3 @@ vim.keymap.set("n", "<leader>ld", vim.lsp.buf.declaration, { desc = "go to decla
 vim.keymap.set({ "n", "v" }, "<leader>lc", vim.lsp.buf.code_action, { desc = "code action" })
 vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "rename all" })
 vim.keymap.set("n", "<leader>ld", vim.lsp.buf.hover, { desc = "documentation" })
-
--- ====================================================================
---                              telescope
--- ====================================================================
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>Tr", "<cmd>Telescope lsp_references<CR>", { desc = "references" })
-vim.keymap.set("n", "<leader>Td", "<cmd>Telescope lsp_definitions<CR>", { desc = "definitions" })
-vim.keymap.set("n", "<leader>Ti", "<cmd>Telescope lsp_implementations<CR>", { desc = "implementations" })
-vim.keymap.set("n", "<leader>Tt", "<cmd>Telescope lsp_type_definitions<CR>", { desc = "type" })
-vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", { desc = "list diagnostics" })
-vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "find files" })
-vim.keymap.set("n", "<leader>st", builtin.live_grep, { desc = "find text" })
-
