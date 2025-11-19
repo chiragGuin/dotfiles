6bc665e75a75b6b1674bae9b6abaa8c5810b524f
chiragGuin <chiragguin2008@gmail.com>
2025-11-19 21:33:15 +0530

    resolve

--- nvim/lua/plugins/dooing.lua
+++ nvim/lua/plugins/dooing.lua
@@ -68,38 +68,6 @@ return {
 				move_completed_to_end = true, -- Move completed nested tasks to end of parent group
 			},
 
-			-- Keymaps
-			keymaps = {
-				toggle_window = "<leader>td", -- Toggle global todos
-				open_project_todo = "<leader>tD", -- Toggle project-specific todos
-				new_todo = "i",
-				create_nested_task = "<leader>tn", -- Create nested subtask under current todo
-				toggle_todo = "x",
-				delete_todo = "d",
-				delete_completed = "D",
-				close_window = "q",
-				undo_delete = "u",
-				add_due_date = "H",
-				remove_due_date = "r",
-				toggle_help = "?",
-				toggle_tags = "t",
-				toggle_priority = "<Space>",
-				clear_filter = "c",
-				edit_todo = "e",
-				edit_tag = "e",
-				edit_priorities = "p",
-				delete_tag = "d",
-				search_todos = "/",
-				add_time_estimation = "T",
-				remove_time_estimation = "R",
-				import_todos = "I",
-				export_todos = "E",
-				remove_duplicates = "<leader>D",
-				open_todo_scratchpad = "<leader>p",
-				refresh_todos = "f",
-				share_todos = "s",
-			},
-
 			calendar = {
 				language = "en",
 				icon = "",
